e6125d4a57a6b579e1a4c7cec70f422280934f8f
Jonathan Davis <jonathan@obj.co>
2018-10-14 00:25:37 -0600

    Connects the routes to the app

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
-var createError = require('http-errors')
-var express = require('express')
-var logger = require('morgan')
+const createError = require('http-errors')
+const express = require('express')
+const logger = require('morgan')
+const routes = require('./routes')
 
-var app = express()
+const app = express()
 
 app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
+
+app.use('/', routes)
 
 app.use(function(req, res, next) {
   next(createError(404))
